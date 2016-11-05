69c701669b265fcd8ac6c8f81c5e47a13059fa8e
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-11-05 16:32:10 -0700

    Added segmented controls

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController:  UIViewController {
+    
+    var mapView : MKMapView!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         print("Map has loaded")
+    }
+    
+    override func loadView() {
+        //create a map view
+        mapView = MKMapView()
+        
+        self.view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+       
     }
     
     
